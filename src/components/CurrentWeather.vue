79e884c17c1fc75aaa8bb20e0e1f9e87e2951d71
Shawn Rider <shawn@shawnrider.com>
2017-10-29 14:19:23 -0700

    Update CurrentWeather.vue

--- src/components/CurrentWeather.vue
+++ src/components/CurrentWeather.vue
@@ -70,6 +70,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.errors li {
+  color: red;
+  border: solid red 1px;
+  padding: 5px;
+}
 h1, h2 {
   font-weight: normal;
 }
